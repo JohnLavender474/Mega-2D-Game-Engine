8a1a187ecc1f56d5a1701dda5d6f1812c7314fa5
JohnLavender474 <johnmichaellavender123@gmail.com>
2024-05-10 07:59:57 -0400

    Switching the ControllerPoller.kt off sets buttons to RELEASED or JUST_RELEASED depending on their current states

--- src/main/com/engine/controller/polling/ControllerPoller.kt
+++ src/main/com/engine/controller/polling/ControllerPoller.kt
@@ -1,6 +1,7 @@
 package com.engine.controller.polling
 
 import com.badlogic.gdx.utils.ObjectMap
+import com.engine.common.objects.computeValues
 import com.engine.controller.ControllerUtils
 import com.engine.controller.buttons.ButtonStatus
 import com.engine.controller.buttons.Buttons
@@ -12,8 +13,15 @@ import com.engine.controller.buttons.Buttons
  */
 open class ControllerPoller(val buttons: Buttons) : IControllerPoller {
 
-    val statusMap = ObjectMap<Any, ButtonStatus>()
+    /**
+     * Whether the poller is on. If the poller is turned off, then keyboard/controller input will not be processed.
+     * In this case, the status of all buttons that are [ButtonStatus.PRESSED] or [ButtonStatus.JUST_PRESSED] will be
+     * set to [ButtonStatus.JUST_RELEASED], and the status of all buttons that are [ButtonStatus.RELEASED] or
+     * [ButtonStatus.JUST_RELEASED] will be set to [ButtonStatus.RELEASED].
+     */
     override var on = true
+
+    private val statusMap = ObjectMap<Any, ButtonStatus>()
 
     init {
         buttons.keys().forEach { statusMap.put(it, ButtonStatus.RELEASED) }
@@ -22,22 +30,18 @@ open class ControllerPoller(val buttons: Buttons) : IControllerPoller {
     override fun getStatus(key: Any): ButtonStatus? = statusMap[key]
 
     override fun run() {
-        if (!on) return
-
         buttons.forEach { e ->
             val key = e.key
             val button = e.value
 
             if (!statusMap.containsKey(key)) statusMap.put(key, ButtonStatus.RELEASED)
-            val status = statusMap.get(key)
+            val status = statusMap.get(key)!!
 
-            var pressed: Boolean
-            button.keyboardCode.let { pressed = ControllerUtils.isKeyboardKeyPressed(it) }
-            if (!pressed)
-                button.controllerCode?.let { pressed = ControllerUtils.isControllerKeyPressed(it) }
-
-            val newStatus =
+            val newStatus: ButtonStatus = if (on) {
                 if (button.enabled) {
+                    var pressed = ControllerUtils.isKeyboardKeyPressed(button.keyboardCode)
+                    if (!pressed && button.controllerCode != null) pressed =
+                        ControllerUtils.isControllerKeyPressed(button.controllerCode!!)
                     when (status) {
                         ButtonStatus.RELEASED,
                         ButtonStatus.JUST_RELEASED ->
@@ -47,6 +51,10 @@ open class ControllerPoller(val buttons: Buttons) : IControllerPoller {
                     }
                 } else if (status == ButtonStatus.JUST_RELEASED) ButtonStatus.RELEASED
                 else ButtonStatus.JUST_RELEASED
+            } else when (status) {
+                ButtonStatus.PRESSED, ButtonStatus.JUST_PRESSED -> ButtonStatus.JUST_RELEASED
+                ButtonStatus.RELEASED, ButtonStatus.JUST_RELEASED -> ButtonStatus.RELEASED
+            }
 
             statusMap.put(key, newStatus)
         }

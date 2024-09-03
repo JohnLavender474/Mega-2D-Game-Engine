d615f9ac3199a564f582541ee55b24af0bf59481
JohnLavender474 <johnmichaellavender123@gmail.com>
2024-09-02 22:50:26 -0400

    Add new classes and tests for state machine. Update version to 1.2.

---
+++ src/main/com/engine/state/State.kt
@@ -0,0 +1,31 @@
+package com.engine.state
+
+import com.badlogic.gdx.utils.Array
+
+/**
+ * A simple state class representing a node in the state machine.
+ *
+ * @param T the type of elements in the state machine
+ */
+class State<T>(val element: T, val transitions: Array<Transition<T>> = Array<Transition<T>>()) {
+
+    /**
+     * Adds a transition to this state that moves to the given state when the condition is met.
+     *
+     * @param condition the condition to check for the transition
+     * @param nextState the next state to transition to
+     */
+    fun addTransition(condition: () -> Boolean, nextState: State<T>) = transitions.add(Transition(condition, nextState))
+
+    /**
+     * Gets the next state based on the conditions of the transitions. The [transitions] array is iterated based on
+     * insertion order. Manipulation of this array should not be done from within any [Transition.condition] or
+     * [Transition.nextState] method logic.
+     *
+     * @return the next state if a condition is met, null otherwise
+     */
+    fun getNextState(): State<T>? {
+        for (transition in transitions) if (transition.condition()) return transition.nextState
+        return null
+    }
+}
